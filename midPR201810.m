468237597a70778cb838ff3d9573da8882beaa4c
XHuang304 <huangxiao@zju.edu.cn>
2018-11-07 17:13:43 +0800

    Updated with Round Test! The Core Nonlinear Iteration Algorithm should be revised in future.

--- midPR201810.m
+++ midPR201810.m
@@ -13,29 +13,56 @@ EffDim=4;
 
 %Amplitude
 pupil=makepupil2017(pixNum,Dim/2,EffDim/2);
-
+% pupil=imread('E:\2018_1\codes\SurfaceDiffraction\zjubw.jpg');
+pupil=imresize(pupil,[pixNum,pixNum]);
+pupil=im2double(pupil);
 %Phase
-zerVec=lambda*[0, 0, 0, 0.9,...
-               0, 0, 0.6, 0.5];%The parameter is corresponding to the parameter of Zemax (Fringe Zernike Phase)
+zerVec=lambda*[0, 0, 0, 0,...
+               0, 0, 0, 0.2];%The parameter is corresponding to the parameter of Zemax (Fringe Zernike Phase)
 HalfDim = EffDim/2;
 NormRadius = HalfDim;
 SagTruth=generateZernike(zerVec, pixNum, HalfDim, NormRadius);
 PhaseTruth = pupil.*exp(-1j*k*SagTruth);
 %% Propagation
-f=150;
+f=364;
 FocalSag = generateFocal(f,Dim,pixNum);
 FocalPhase = exp(1j*k*FocalSag);
 
+
+% S=3;
+% w=1;
+% FocalPhase=generateFractalZP(S,w,pixNum,Dim/2);
+
+
+
 ComplexToProp = PhaseTruth.*FocalPhase;
-z_prop=[130,140,160];
+% z_prop=[236,252,284,308,567];%系列次焦点
+% z_prop=[234,238,250,254,282,286,306,310];
+% z_prop=[200,250,310,375,450];
+% z_prop=[222,258,296,415,485];%系列亮点
+% z_prop=[216,234,248,268,461];%系列暗点
+z_prop=[358,370,119,123,331,403];%系列亮点
+% z_prop=[358,370,376,352,382,346];%系列亮点
+
 
 imgNum = numel(z_prop);
 Iout = cell(1,imgNum);
+% IoutMaxTotal=0;
 for i = 1:imgNum
-    Iout{i}=abs(ASMDiffgpu(ComplexToProp,z_prop(i),lambda,Dim)).^2;
+    Iout{i}=abs(ASMDiffgpu(ComplexToProp,z_prop(i),lambda,Dim)).^2;    
+    Iout{i}=round(Iout{i});
+%     if (max(max(Iout{i}))>IoutMaxTotal)
+%         IoutMaxTotal=max(max(Iout{i}));
+%     end
 end
-PSF=abs(ASMDiffgpu(ComplexToProp,f,lambda,Dim)).^2;
+% Iout=double2grayscale(Iout);
 
+%% Add Noise
+% Iout{i}=imnoise(Iout{i},'poisson');
+    
+PSF=abs(ASMDiffgpu(ComplexToProp,f,lambda,Dim)).^2;
+% z_prop=z_prop+(rand(1,imgNum)-0.5)*0.2;%Z shift error
+% z_prop=z_prop+0.5;%Z shift error
 %% Preprocession of Intensity Data
 ImgMeasured = Iout;
 % ImgMeasured = Iin;
@@ -57,11 +84,12 @@ E_Update = E_zeros;
 
 CostFuncTemp = zeros(1,imgNum);
 %% Initial Figure
-figNum=2;
+figNum=1;
 figure(figNum)   % create figure for displaying the result through out the iteration
 % colormap(gray)
 subplot(2,2,1)
-imagesc(unwrap(angle(PhaseTruth)))
+% imagesc(unwrap_phase(angle(PhaseTruth)))
+imagesc(k*SagTruth.*pupil)
 title('Phase Groundtruth');
 axis off
 axis equal
@@ -81,11 +109,12 @@ axis equal
 %     FocalSag_Init = generateFocal(f_Init,Dim,pixNum);
 %     FocalPhase_Init = exp(1j*k*FocalSag_Init);
     %% Initializing to Zernike Surface
-    zerVec_Init=lambda*[0, 0, 0, 0.9,...
-                   0, 0, 0.6, 0.5];%The parameter is corresponding to the parameter of Zemax (Fringe Zernike Phase)
+    zerVec_Init=lambda*[0, 0, 0, 0,...
+                   0, 0, 0, 0];%The parameter is corresponding to the parameter of Zemax (Fringe Zernike Phase)
 
     Sag_Init=generateZernike(zerVec_Init, pixNum, HalfDim, NormRadius);
-    Phase_Init = pupil.*exp(-1j*k*Sag_Init);
+%     Phase_Init = pupil.*exp(-1j*k*Sag_Init);
+Phase_Init = exp(-1j*k*Sag_Init);
 
 
 % Pupil_Init=makepupil2017(pixNum,Dim/2,EffDim/2);
@@ -93,7 +122,7 @@ Pupil_Init=ones(pixNum,pixNum);
 E_Obj_Init = Phase_Init.*Pupil_Init;
 E_Obj_Result = E_Obj_Init.*FocalPhase;
 %% Iteration Settings
-iterNum=60;	% The number of iterations to be calculated
+iterNum=30;	% The number of iterations to be calculated
 CostFunc = ones(1,iterNum);
 E_theta_Gradient_Last_Iter=cell(1,imgNum);
 for ii=1:imgNum
@@ -103,12 +132,15 @@ E_theta_Gradient = E_zeros;
 Dq_Last_Iter=E_zeros;
 Dq=E_zeros;
 
-p=1; % The counter of the loop
+p=1; % The counter of the loop 
+CostFuncTemp(1)=norm((abs(ASMDiffgpu(E_Obj_Result,z_prop(1),lambda,Dim))-sqrt(ImgMeasured{1})),2);
+CostFunc(p)=sum(CostFuncTemp);
+disp([num2str(CostFunc(p)),'(',num2str(p),')']);
 while p < iterNum&&(CostFunc(p)>1e-4)	% The number of iterations that will be
                                     % calculated (This method converge within
                                     % approximately 10-50 iterations).
     p = p+1;      % Increase the counter        
-
+%     z_prop=z_prop+(rand(1,imgNum)-0.5)*0.2;%Z shift error
 % 2006 Nonlinear
 %% Procedure 1
     E_Diff_f = ASMDiffgpu(E_Obj_Result,f,lambda,Dim);
@@ -129,6 +161,7 @@ while p < iterNum&&(CostFunc(p)>1e-4)	% The number of iterations that will be
         end
 %         E_Diff{imgCount}=E_Diff_sum{imgCount}/(imgNum-1);
         E_Diff{imgCount}=sqrt(ImgMeasured{imgCount}).*(E_Diff{imgCount}./abs(E_Diff{imgCount}));
+        E_Diff{imgCount}(isnan(E_Diff{imgCount}))=0;
         
         E_theta_Gradient{imgCount}=2*imag(E_Diff{imgCount}.*E_Diff_sum{imgCount});
         
@@ -140,7 +173,7 @@ while p < iterNum&&(CostFunc(p)>1e-4)	% The number of iterations that will be
 
         
         if imgCount<imgNum
-%             E_Update{imgCount}=real(E_Diff{imgCount})+1j*(imag(E_Diff{imgCount})-0.001*Dq{imgCount});
+%             E_Update{imgCount}=real(E_Diff{imgCount})+1j*(imag(E_Diff{imgCount})-0.000*Dq{imgCount});
             E_Update{imgCount}=abs(E_Diff{imgCount}).*exp(1j*(angle(E_Diff{imgCount})+0.0003*Dq{imgCount}));
 %             E_Update{imgCount}=abs(E_Diff{imgCount}).*exp(1j*(angle(E_Diff{imgCount})));
             E_Diff{imgCount+1}=ASMDiffgpu(E_Update{imgCount},z_prop(imgCount+1)-z_prop(imgCount),lambda,Dim);
@@ -158,8 +191,9 @@ while p < iterNum&&(CostFunc(p)>1e-4)	% The number of iterations that will be
     %% Synthesize New Object Wavefront
     
     Phase_Obj_Result = angle(E_Obj_Result);
-    Amp_Obj_Result = abs(E_Obj_Result);
-    Amp_Obj_Result(Amp_Obj_Result<1e-6)=0;
+%     Amp_Obj_Result = abs(E_Obj_Result);
+    Amp_Obj_Result = real(E_Obj_Result./exp(1j*angle(E_Obj_Result)));
+    Amp_Obj_Result(Amp_Obj_Result<1e-5)=0;
     E_Obj_Result = Amp_Obj_Result.*exp(1j*Phase_Obj_Result);
 %     Phase_Obj_Result = angle(E_Obj_Result).*pupil;
 %     Amp_Obj_Result = abs(E_Obj_Result).*pupil;
@@ -169,7 +203,7 @@ while p < iterNum&&(CostFunc(p)>1e-4)	% The number of iterations that will be
     
     CostFuncTemp(1)=norm((abs(E_Diff_test)-sqrt(ImgMeasured{1})),2);
     CostFunc(p)=sum(CostFuncTemp);
-    disp([num2str(CostFunc(p)),'(',num2str(p),')']);
+%     disp([num2str(CostFunc(p)),'(',num2str(p),')']);
     
 %% Plot Current Results
     figure(figNum)    
@@ -181,14 +215,14 @@ while p < iterNum&&(CostFunc(p)>1e-4)	% The number of iterations that will be
     
     figure(figNum)
     subplot(2,2,3)								% Plot the phase levels
-    imagesc(angle(Phase_Obj_Result./(E_Obj_Result/k)));
-    title('MSF Error');
+    imagesc(Amp_Obj_Result);
+    title('Recovered Amplitude');
     axis equal
     axis off
 
     figure(figNum)
     subplot(2,2,4)					% Calculate and plot the intensity quotient as a 
-    plot((2:p),CostFunc(2:p),'b.-')	    % function of the number of iterations
+    plot((1:p),CostFunc(1:p),'b.-')	    % function of the number of iterations
     axis([1 iterNum -0.01 max(CostFunc)])
     xlabel('Iteration Times')
     ylabel('Least Square Error')    
@@ -198,7 +232,34 @@ while p < iterNum&&(CostFunc(p)>1e-4)	% The number of iterations that will be
     
     
 end
+figure(figNum)    
+subplot(2,2,2)								% Plot the phase levels
+imagesc(unwrap_phase((Phase_Obj_Result-angle(FocalPhase)).*Amp_Obj_Result));
+title('Recovered Phase');
+axis equal
+axis off
+    
 toc
-final=CostFunc(end)
+final=CostFunc(p)
 
 % RMS=sqrt(sum(sum((unwrap((angle(E_Obj_Result)-angle(FocalPhase)).*(abs(E_Obj_Result)>1e-6)))-unwrap(angle(PhaseTruth))).^2))/pixNum^2;
+
+function Ig=double2grayscale(Iin)
+    %% Gray Scale
+    GrayScale=4096;
+    if iscell(Iin)
+        imgNum=length(Iin);
+        IoutMaxTotal=0;
+        for i = 1:imgNum
+            if max(max(Iin{i}))>IoutMaxTotal
+                IoutMaxTotal=max(max(Iin{i}));
+            end
+        end
+        for i = 1:imgNum
+            Iin{i}=round(Iin{i}/IoutMaxTotal*GrayScale);
+        end
+    else
+        Iin=round(Iin/max(max(Iin))*GrayScale);
+    end
+    Ig=Iin;
+end
